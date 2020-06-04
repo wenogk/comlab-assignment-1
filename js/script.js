66fd53e240cf3bb3d45d105f366d1036cd6ee6a9
wenogk <drf325@nyu.edu>
2020-06-04 13:09:02 +0400

    updates

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 
 const signupButtonClicked = () => {
+
   let formContainer = document.getElementById("formContainer");
   let name = document.getElementById("nameInputValue").value;
-  formContainer.style.display = "none";
   let loaderContainer = document.getElementById("loaderContainer");
   let successContainer = document.getElementById("successContainer");
+  
+  formContainer.style.display = "none";
   loaderContainer.style.display = "block";
-  setTimeout(() => {  
+
+  setTimeout(() => {
     loaderContainer.style.display = "none";
     document.getElementById("nameSpan").innerHTML = name;
     successContainer.style.display = "block";
   }, 3000);
-  
+
 }
 const signUpButton = document.getElementById("signUpButton");
 
