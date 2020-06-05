5ce084f91fc3bcedd952c538b3059b63c377ef8d
wenogk <drf325@nyu.edu>
2020-06-06 01:38:41 +0400

    updates

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const signupButtonClicked = () => {
   let name = document.getElementById("nameInputValue").value;
   let loaderContainer = document.getElementById("loaderContainer");
   let successContainer = document.getElementById("successContainer");
-  
+
   formContainer.style.display = "none";
   loaderContainer.style.display = "block";
 
@@ -13,9 +13,10 @@ const signupButtonClicked = () => {
     loaderContainer.style.display = "none";
     document.getElementById("nameSpan").innerHTML = name;
     successContainer.style.display = "block";
+    formContainer.style.display = "block";
   }, 3000);
 
 }
-const signUpButton = document.getElementById("signUpButton");
+const signUpButton = document.getElementById("signUpButton"); //variable to selector of sign up button
 
-signUpButton.addEventListener("click", signupButtonClicked);
+signUpButton.addEventListener("click", signupButtonClicked); // onClick event listener for when sign up button is clicked
